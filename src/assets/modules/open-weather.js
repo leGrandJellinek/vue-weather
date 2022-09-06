052be8ad4c1a1aaac4320b1f50dd450247022af9
Jellinek <jellinekbraus@gmail.com>
2022-09-07 01:11:57 +0500

    second commit

--- src/assets/modules/open-weather.js
+++ src/assets/modules/open-weather.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 async function  getWeather(appId,cityName) {
     try {
-        const result = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${appId}`);
+        const result = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${appId}`);
         const city = result.data[0]
         let {lat, lon, name: name} = city;
         const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&exclude=hourly,alerts,minutely&units=metric&lang=ru&appid=${appId}`)
